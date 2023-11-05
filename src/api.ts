5a4ddcd8e32e112ed1972e4fb8f564ac5780041a
ARGI-BERRI <me@endroit.net>
2023-11-05 18:36:17 +0900

    feat(api): add getDeviceStatus()

--- src/api.ts
+++ src/api.ts
@@ -3,19 +3,35 @@ import { generateHeader } from "./signature";
 
 const BASE_URL = "https://api.switch-bot.com/v1.1";
 
-export async function getDevices() {
-  const endpoint = `${BASE_URL}/devices`;
-  const headers = generateHeader();
-
+async function fetchAPI(endpoint: string) {
   log(`Connecting to ${endpoint}`);
 
-  const response = await fetch(endpoint, {
-    headers,
-  });
+  const headers = generateHeader();
+  const response = await fetch(endpoint, { headers });
+  const json = (await response.json()).body;
 
-  const json = (await response.json()).body.deviceList;
+  return json;
+}
 
-  const devices = JSON.stringify(json, null, "  ");
+// https://github.com/OpenWonderLabs/SwitchBotAPI?tab=readme-ov-file#devices
+interface Device {
+  deviceId: string;
+  deviceName: string;
+  deviceType: string;
+  enableCloudService: boolean;
+  hubDeviceId: string;
+}
+
+export async function getDevices(): Promise<Device[]> {
+  const endpoint = `${BASE_URL}/devices`;
+  const devices = (await fetchAPI(endpoint)).deviceList;
 
   return devices;
+}
+
+export async function getDeviceStatus(deviceId: string) {
+  const endpoint = `${BASE_URL}/devices/${deviceId}/status`;
+  const json = await fetchAPI(endpoint);
+
+  return json;
 }
